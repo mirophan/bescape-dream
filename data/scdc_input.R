2b20f403aef14f1db4f0d0e31755bd4b2bd862e5
tkamth <tkamth@gmail.com>
2020-04-30 21:59:22 +0200

    recalibration for missing cell types

--- data/scdc_input.R
+++ data/scdc_input.R
@@ -38,7 +38,7 @@ do_scdc <- function(expression_path, dataset_name){
         tibble::column_to_rownames("Gene") %>% 
         as.matrix()
 
-    #creating phenoData with just sample ID
+    #creating phenoData with just sample ID (TKT)
     pData<-as.data.frame(colnames(expression_matrix))
     colnames(pData)<-"sample.id"
     metadata<-data.frame(labelDescription=c("sample.id"),row.names=c("sample.id"))
@@ -50,21 +50,20 @@ do_scdc <- function(expression_path, dataset_name){
     # scdc deconvolution - doesnt need to run, just need to make sure the bulk.eset is in the correct format
     result_matrix <- SCDC_ENSEMBLE(bulk.eset = bulk, sc.eset.list = scdata, ct.varname = params$celltypevar, sample = params$samplevar, truep = NULL, ct.sub =  celltypesel, search.length = 0.01, grid.search = T)
     
-    # Convert the result matrix back to a dataframe
-    result_df <- result_matrix %>% 
-        as.data.frame() %>% 
-        tibble::rownames_to_column("mcpcounter.cell.type") %>% 
-        dplyr::as_tibble()
-    result_df <- SCDC
+    #For missing celltypes: Run based on intercept of cell types across scRNASeq datasets and correct
+    recalib<-function(resultmat,cells_zero,cell_sum,cells_add){
+        sumind<-which(colnames(resultmat) %in% cells_add)
+        resultmat<-resultmat %>% dplyr::mutate(!!cells_zero:=0) %>%
+            dplyr::mutate(!!cell_sum:=rowSums(resultmat[,sumind]))
+        return(resultmat/rowSums(resultmat))#recalibrate based on new Rowsum to add up to one
+    }
+    #Example
+    result_matrix<-recalib(result_matrix,"neutrophils","monocytes",c("macrophages","myeloid.dentritic.cells"))
+    #result_matrix$neutrophils<-0 #rm hard coded version
+    #result_matrix$monocytes<-result_matrix$macrophages+result_matrix$myeloid.dendritic.cells#rm hard coded version
     
-    # Stack the predictions into one column
-    result_df <- tidyr::gather(
-        result_df,
-        key = "sample.id", 
-        value = "prediction", 
-        -mcpcounter.cell.type) 
-
-    #For SCDC based on current output/predictions.csv
+    
+    #For SCDC based on current output/predictions.csv(TKT)
     #1) put the sample into column, use as key
     #2) convert wide to long format using all the remaining variables(cell types)   
     result_df_wide<- result_matrix %>% 
